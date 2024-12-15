5eb3848ccb294ca9580da76bdcb813b96ef49709
ICTU-Loren <99655639+ICTU-Loren@users.noreply.github.com>
2024-12-15 18:39:34 +0800

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
   <footer class="main-footer h-14">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      Electronic Document Tracking System | Anti-Red Tape Authority
+      Electronic Document Management System | Anti-Red Tape Authority
     </div>
     <!-- Default to the left -->
     <!-- <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved. -->

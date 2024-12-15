0a1a2bdfed440561708523286f143d2b6fd45edb
ICTU-Loren <99655639+ICTU-Loren@users.noreply.github.com>
2024-12-15 18:32:26 +0800

    Update forward-docs.blade.php

--- resources/views/livewire/files/forward/forward-docs.blade.php
+++ resources/views/livewire/files/forward/forward-docs.blade.php
@@ -5,12 +5,12 @@
             <form wire:submit.prevent="submit" method="post" enctype="multipart/form-data">
 
                 <!-- Addressed to --> 
-                <p class="text-primary mb-3">ASSIGNED TO</p>
+                <h6 class="text-primary mb-3">ASSIGNED TO</h6>
                     <div class="form-row">
                         
                         <div class="form-group col-md-3">
-                            <label for="department">Head Office *</label>
-                            <select wire:model="selectedDepartment" wire:model.defer="department" class="form-control rounded-0">
+                            <label for="department">Head Office <code>*</code></label>
+                            <select wire:model="selectedDepartment" wire:model.defer="department" class="form-control">
                                 <option value="" selected>-</option>
                                 
                                     @foreach($departments as $department)
@@ -24,8 +24,8 @@
                         </div>
 
                         <div class="form-group col-md-3">
-                            <label for="office">Office/Unit *</label>
-                            <select wire:model="selectedOffice" wire:model.defer="office" class="form-control rounded-0">
+                            <label for="office">Office/Unit <code>*</code></label>
+                            <select wire:model="selectedOffice" wire:model.defer="office" class="form-control">
                                 <option value="" selected>-</option>
                                 @if (!is_null($selectedDepartment))
                                     @foreach($offices as $office)
@@ -39,8 +39,8 @@
                         </div>
                         
                         <div class="form-group col-md-3">
-                            <label for="div_unit">Director/Division/Personnel *</label>
-                            <select wire:model="selectedDivunit" wire:model.defer="div_unit" class="form-control rounded-0">
+                            <label for="div_unit">Director/Division/Designation <code>*</code></label>
+                            <select wire:model="selectedDivunit" wire:model.defer="div_unit" class="form-control">
                                 <option value="" selected>-</option>
                                 @if (!is_null($selectedOffice))
                                     @foreach($div_units as $div_unit)
@@ -54,7 +54,7 @@
                         </div>
 
                         <div class="form-group col-md-3">
-                            <label for="personnel">Name *</label>
+                            <label for="personnel">Name <code>*</code></label>
                             <select wire:model="selectedPersonnel" wire:model.defer="personnel" name="personnels_id" class="form-control rounded-0">
                                 <option value="" selected>-</option>
                                 @if (!is_null($selectedDivunit))
@@ -69,7 +69,7 @@
                         </div>
 
                         <div class="form-group col-md-3" hidden>
-                            <label for="email">Email Address*</label>
+                            <label for="email">Email Address <code>*</code></label>
                             <select wire:model.defer="email" name="emails_id" class="form-control rounded-0">
                                 @if (!is_null($selectedPersonnel))
                                     @foreach($emails as $email)
@@ -84,26 +84,35 @@
                         
                     </div>
 
-                    <!-- Subject -->
+                    <!-- Subject, Remarks / Instructions -->
                     <div class="form-row">
-                        <div class="form-group col-md-12">
-                            <label for="subject">Title *</label>
-                            <textarea class="form-control rounded-0" id="subject" rows="2" wire:model="subject"></textarea>
-                                <!-- <ul class="text-muted well well-sm shadow-none">
-                                    <li>- You may remove any sensitive information (monetary amounts, names, etc.) if they are not necessary in tracking the document.</li>
+                        <div class="form-group col-md-6">
+                            <label for="subject">Subject <code>*</code></label>
+                            <textarea class="form-control" id="subject" rows="3" wire:model="subject"></textarea>
+                                <ul class="text-muted well well-sm shadow-none">
                                     <li>- Max Length: 1000 characters</li>
-                                </ul> -->
+                                    <li>- You may remove any sensitive information (monetary amounts, names, etc.)</li>
+                                    <li>* if they are not necessary in tracking the document.</li>
+                                </ul>
                                 @error('subject')
                                     <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                                 @enderror
                         </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="note_comment_routed">Remarks / Instructions</label>
+                            <textarea class="form-control" name="note_comment_routed" id="note_comment_routed" wire:model="note_comment_routed" rows="3"></textarea>
+                                <ul class="text-muted well well-sm shadow-none">
+                                    <li>- Max Length: 1000 characters</li>
+                                </ul>
+                        </div>
                     </div>
 
-                    <!-- Action, Due, Email -->
+                    <!-- Action, Deadline, Email -->
                     <div class="form-row">
                         <div class="form-group col-md-4">
-                            <label for="action_req">Action Required *</label>
-                            <select wire:model.defer="action_req" name="action_req" class="form-control rounded-0">
+                            <label for="action_req">Action Requested <code>*</code></label>
+                            <select wire:model.defer="action_req" name="action_req" class="form-control">
                                 <option value="" selected>-</option>
                                     @foreach ($actions as $action_req)
                                         <option value="{{ $action_req }}">{{ $action_req }}</option>
@@ -115,7 +124,7 @@
                         </div>
 
                         <div class="form-group col-md-4">
-                            <label>Due Date</label>
+                            <label>Deadline</label>
                             <div wire:ignore class="input-group date" id="docDateRoute" data-target-input="nearest" data-docdateroute="@this">
                                 <input type="text" class="form-control datetimepicker-input" data-target="#docDateRoute" id="docdaterouteInput">
                                 <div class="input-group-append" data-target="#docDateRoute" data-toggle="datetimepicker">
@@ -126,8 +135,8 @@
                         </div>
 
                         <div class="form-group col-md-4">
-                            <label for="modeoftrans">Mode of Transmittal *</label>
-                            <select wire:model.defer="modeoftrans" name="modeoftrans" class="form-control rounded-0">
+                            <label for="modeoftrans">Mode of Transmittal <code>*</code></label>
+                            <select wire:model.defer="modeoftrans" name="modeoftrans" class="form-control">
                                 <option value="" selected>-</option>
                                     @foreach ($exmodeoftrans as $modeoftrans)
                                         <option value="{{ $modeoftrans }}">{{ $modeoftrans }}</option>
@@ -145,11 +154,11 @@
                         </div> -->
                     </div>
 
-                    <!-- Comments, File -->
+                    <!-- Upload File, Sharepoint -->
                     <div class="form-row">
-                        <div class="form-group col-md-4">
+                        <div class="form-group col-md-6">
                             <label for="file_upload">Upload file </label> <div wire:loading wire:target="file_upload"><span style="color:#007bff; font-weight:500;"><i class="fas fa-upload"></i> Uploading...</i></span></div>
-                            <input type="file" class="form-control rounded-0" name="file_upload" wire:model="file_upload">
+                            <input type="file" class="form-control" name="file_upload" wire:model="file_upload">
                                 <ul class="text-muted well well-sm shadow-none">
                                     <li>- Allowed formats: PDF, DOCX, JPG, PNG</li>
                                     <li>- Maximum Size: 10MB</li>
@@ -162,30 +171,45 @@
                         <div class="form-group col-md-4">
                             <label for="note_comment">SharePoint Link</label>
                             <div class="input-group">
-                                <input type="text" class="form-control rounded-0" id="sharepoint_link" wire:model="sharepoint_link">
+                                <input type="text" class="form-control" id="sharepoint_link" wire:model="sharepoint_link">
                                 <div class="input-group-prepend"></div>
                                     <ul class="text-muted well well-sm shadow-none">
                                         <li>- You can upload files via <a href="#">SharePoint</a> and then input the link here.</li>
                                     </ul>
-                            </div>
-                                @error('sharepoint_link')
-                                    <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                                @enderror
+                                    @error('sharepoint_link')
+                                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                                    @enderror
+                                </div>
                         </div>
 
-                        <div class="form-group col-md-4">
-                            <label for="note_comment_routed">Notes / Comments</label>
-                            <textarea class="form-control rounded-0" name="note_comment_routed" id="note_comment_routed" wire:model="note_comment_routed" rows="3"></textarea>
-                                <ul class="text-muted well well-sm shadow-none">
-                                    <li>- Max Length: 500 characters</li>
-                                </ul>
-                        </div>
                     </div>
                     
                     <div class="card-footer mt-6">
                         <button type="reset" class="btn btn-secondary btn-sm">Clear all</button>
-                        <button type="submit" class="btn btn-primary btn-sm"><i class="fas fa-share-square"></i> Submit</button>
+                        <a data-toggle="modal" href="#routeExternalSubmit" class="btn btn-primary btn-sm"><i class="fas fa-share-square"></i> Submit</a>
                     </div>
+
+                    <!-- Modal for Submit Button -->
+                    <div class="modal fade" id="routeExternalSubmit" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-md" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header bg-danger">
+                                    <h5 class="modal-title" id="ModalLabel">Submit Route</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Make sure to select the correct Assigned Personnel and File before confirming. Do you want to submit?</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary btn-sm close-btn float-right" data-dismiss="modal">Cancel</button>
+                                    <button type="submit" class="btn btn-danger btn-sm"> Confirm</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
             </form>
 
         </div>
@@ -198,8 +222,8 @@
     });
 </script>
 
-<script>
+<!-- <script>
     $('#exampleModal').on('hidden.bs.modal', function () { 
     location.reload();
 });
-</script>
+</script> -->

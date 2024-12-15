89c4bef90bfeccaa52c6e6e4947af68c0bb2383a
ICTU-Loren <99655639+ICTU-Loren@users.noreply.github.com>
2024-12-15 18:21:46 +0800

    Update create-new.blade.php

--- resources/views/livewire/docs/e/create-new.blade.php
+++ resources/views/livewire/docs/e/create-new.blade.php
@@ -13,10 +13,26 @@
 
             <!-- Recipient --> 
             <p class="text-primary my-3"> ADDRESS TO</p>
+
+            <!-- Personnel (For) -->
             <div class="form-row">
+                <div wire:ignore class="form-group col-md-3">
+                    <label for="personnel">For</label>
+                    <select wire:model="personnel" class="form-control select2" id="personnel">
+                        <option value="" selected>-</option>    
+                            @foreach($personnels as $personnel)
+                                <option value="{{ $personnel->name }} / ({{ $personnel->div_unit }})">{{ $personnel->name }} ({{ $personnel->div_unit}})</option>
+                            @endforeach
+                    </select>
+                    @error('personnel')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <!-- Department -->
                 <div class="form-group col-md-3">
-                    <label for="departments">Head Office *</label>
-                    <select wire:model="selectedDepartment" wire:model.defer="department" class="form-control rounded-0" >
+                    <label for="department">and/or <span style="margin-left:10px;">Head Office</span></label>
+                    <select wire:model="selectedDepartment" wire:model.defer="department" class="form-control" id="department">
                         <option value="" selected>-</option>
                         @foreach($departments as $department)
                             <option value="{{ $department->id }}">{{ $department->name }}</option>
@@ -27,31 +43,34 @@
                     @enderror
                 </div>
 
+                <!-- Office/Unit -->
+                @if (!is_null($selectedDepartment))
                 <div class="form-group col-md-3">
                     <label for="office">Office/Unit</label>
-                    <select wire:model="selectedOffice" wire:model.defer="office" class="form-control rounded-0" >
+                    <select wire:model="selectedOffice" wire:model.defer="office" class="form-control" id="office">
                         <option value="" selected>-</option>
-                        @if (!is_null($selectedDepartment))
                             @foreach($offices as $office)
-                                <option value="{{ $office->id }}">{{ $office->name }}</option>
-                            @endforeach
-                        @endif
+                                <option value="{{ $office->id }}">{{ $office->name}}</option>
+                            @endforeach 
                     </select>
                 </div>
+                @endif
                 
+                <!-- Director/Division/Personnel -->
+                @if (!is_null($selectedOffice))
                 <div class="form-group col-md-3">
-                    <label for="div_unit">Director/Division/Personnel</label>
-                    <select wire:model.defer="div_unit" class="form-control rounded-0">
+                    <label for="div_unit">Director/Division/Designation</label>
+                    <select wire:model.defer="div_unit" class="form-control" id="div_unit">
                         <option value="" selected>-</option>
-                        @if (!is_null($selectedOffice))
                             @foreach($div_units as $div_unit)
                                 <option value="{{ $div_unit->name }}">{{ $div_unit->name }}</option>
                             @endforeach
-                        @endif
                     </select>
                 </div>
+                @endif
 
-                <div class="form-group col-md-3">
+                <!-- Original Name -->
+                <!-- <div class="form-group col-md-3"> -->
                     <!-- <label for="personnel">Personnel</label>
                     <select wire:model="selectedPersonnel" wire:model.defer="personnel" name="personnels_id" class="form-control rounded-0">
                         <option value="" selected>-</option>
@@ -62,7 +81,7 @@
                         @endif
                     </select> -->
 
-                    <label for="personnel">Name *</label>
+                    <!-- <label for="personnel">Name *</label>
                     <div class="input-group">
                         <input type="text" class="form-control rounded-0" id="personnel" wire:model="personnel">
                         <div class="input-group-prepend"></div>
@@ -70,14 +89,14 @@
                         @error('personnel')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
-                </div>
+                </div> -->
             </div>
 
             <!-- Type & Class -->
             <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="doc_type">Type *</label>
-                    <select wire:model.defer="doc_type" name="doc_type" class="form-control rounded-0">
+                <div wire:ignore class="form-group col-md-6">
+                    <label for="doc_type">Type <code>*</code></label>
+                    <select wire:model.defer="doc_type" name="doc_type" class="form-control select2" id="doc_type">
                         <option value="" selected>-</option>
                         @foreach ($doctype as $doc_type)
                             <option value="{{$doc_type}}">{{$doc_type}}</option>
@@ -88,9 +107,9 @@
                     @enderror
                 </div>
 
-                <div class="form-group col-md-6">
-                    <label for="doc_class">Classification *</label>
-                    <select wire:model.defer="doc_class" name="doc_class" class="form-control rounded-0">
+                <div wire:ignore class="form-group col-md-6">
+                    <label for="doc_class">Classification <code>*</code></label>
+                    <select wire:model.defer="doc_class" name="doc_class" class="form-control select2" id="doc_class">
                         <option value="" selected>-</option>
                             @foreach ($docclass as $doc_class)
                                 <option value="{{$doc_class}}">{{$doc_class}}</option>
@@ -104,21 +123,20 @@
 
             <!-- Title & Subject -->
             <div class="form-row">
-                <div class="form-group col-md-6">
+                <!-- <div class="form-group col-md-6">
                     <label for="title">Title *</label>
                     <textarea class="form-control rounded-0" id="title" rows="3" wire:model="title"></textarea>
                         @error('title')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
-                </div>
+                </div> -->
 
-                <div class="form-group col-md-6">
-                    <label for="subject">Subject *</label>
-                    <textarea class="form-control rounded-0" id="subject" rows="3" wire:model="subject"></textarea>
+                <div class="form-group col-md-12">
+                    <label for="subject">Subject <code>*</code></label>
+                    <textarea class="form-control" id="subject" rows="3" wire:model="subject"></textarea>
                         <ul class="text-muted well well-sm shadow-none">
                             <li>- Max Length: 1000 characters</li>
-                            <li>- You may remove any sensitive information (monetary amounts, names, etc.)</li>                            
-                            <li>* if they are not necessary in tracking the document.<li>
+                            <li>- You may remove any sensitive information (monetary amounts, names, etc.). If they are not necessary in tracking the document.</li>
                         </ul>
                         @error('subject')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
@@ -135,9 +153,9 @@
 
             <!-- MOD & Profile -->
             <div  class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="mode_of_trans">Mode of Transmittal</label>
-                    <select wire:model.defer="mode_of_trans" name="mode_of_trans" class="form-control rounded-0">
+                <div wire:ignore class="form-group col-md-6">
+                    <label for="mode_of_trans">Mode of Transmittal <code>*</code></label>
+                    <select wire:model.defer="mode_of_trans" name="mode_of_trans" class="form-control select2" id="mode_of_trans">
                         <option value="" selected>-</option>
                             @foreach ($modeoftrans as $mode_of_trans)
                                 <option value="{{ $mode_of_trans }}">{{ $mode_of_trans }}</option>
@@ -148,9 +166,9 @@
                     @enderror
                 </div>
 
-                <div class="form-group col-md-6">
-                    <label for="s_profile">Profile</label>
-                    <select wire:model.defer="s_profile" name="s_profile" class="form-control rounded-0">
+                <div wire:ignore class="form-group col-md-6">
+                    <label for="s_profile">Profile <code>*</code></label>
+                    <select wire:model.defer="s_profile" name="s_profile" class="form-control select2" id="s_profile">
                         <option value="" selected>-</option>
                             @foreach($sprofile as $s_profile)
                                 <option value="{{ $s_profile }}">{{ $s_profile }}</option>
@@ -165,16 +183,16 @@
             <!-- Name & Address -->
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <label for="s_name">Name *</label>
-                    <textarea type="text" class="form-control rounded-0" id="s_name" rows="3" wire:model="s_name"></textarea>
+                    <label for="s_name">Name <code>*</code></label>
+                    <textarea type="text" class="form-control" id="s_name" rows="3" wire:model="s_name"></textarea>
                         @error('s_name')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
                 </div>
 
                 <div class="form-group col-md-6">
-                    <label for="s_address">Address *</label>
-                    <textarea type="text" class="form-control rounded-0" id="s_address" rows="3" wire:model="s_address"></textarea>
+                    <label for="s_address">Address</label>
+                    <textarea type="text" class="form-control" id="s_address" rows="3" wire:model="s_address"></textarea>
                         @error('s_address')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
@@ -186,7 +204,7 @@
                 <div class="form-group col-md-6">
                     <label for="s_email">Email Address</label>
                     <div class="input-group">
-                        <input type="text" class="form-control rounded-0" id="s_email" wire:model="s_email">
+                        <input type="text" class="form-control" id="s_email" wire:model="s_email">
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="fas fa-envelope"></i></span>
                             </div>
@@ -197,7 +215,7 @@
                 <div class="form-group col-md-6">
                     <label for="s_contact">Contact Number</label>
                     <div class="input-group">
-                        <input wire:model="s_contact" type="text" class="form-control rounded-0" id="s_contact" >
+                        <input wire:model="s_contact" type="text" class="form-control" id="s_contact" >
                         <div class="input-group-prepend">
                             <span class="input-group-text"><i class="fas fa-phone"></i></span>
                         </div>
@@ -216,7 +234,7 @@
             <!-- Date & Time -->
             <div class="form-row">
                 <div class="form-group col-md-6">
-                <label>Date Received *</label>
+                <label>Date Received <code>*</code></label>
                     <div wire:ignore class="input-group date" id="docDateReceived" data-target-input="nearest" data-docdatereceived="@this">
                         <input type="text" class="form-control datetimepicker-input" data-target="#docDateReceived" id="docdatereceivedInput">
                         <div class="input-group-append" data-target="#docDateReceived" data-toggle="datetimepicker">
@@ -229,7 +247,7 @@
                 </div>
 
                 <div class="form-group col-md-6">
-                <label>Time Received *</label>
+                <label>Time Received <code>*</code></label>
                     <div wire:ignore class="input-group date" id="docTimeReceived" data-target-input="nearest" data-doctimereceived="@this">
                     <input type="text" class="form-control datetimepicker-input" data-target="#docTimeReceived" id="doctimereceivedInput" >
                         <div class="input-group-append" data-target="#docTimeReceived" data-toggle="datetimepicker">
@@ -244,22 +262,22 @@
 
             <!-- Name and Comment -->
             <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="received_by">Received by *</label>
-                    <select type="text" class="form-control rounded-0" id="received_by" rows="1" wire:model="received_by"></textarea>
+                <div wire:ignore class="form-group col-md-6">
+                    <label for="received_by">Received by <code>*</code></label>
+                    <select wire:model="received_by" class="form-control select2" id="received_by">
                         <option value="" selected>-</option>    
                             @foreach($personnels as $personnel)
-                                <option value="{{ $personnel->name }} ({{ $personnel->div_unit }})">{{ $personnel->div_unit }} - {{ $personnel->name }}</option>
+                                <option value="{{ $personnel->name }} ({{ $personnel->div_unit }})">{{ $personnel->name }} / {{ $personnel->div_unit }}</option>
                             @endforeach
                     </select>
-                        @error('received_by')
-                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                        @enderror
+                    @error('received_by')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
                 </div>
 
                 <div class="form-group col-md-6">
                     <label for="comment">Remarks</label>
-                    <textarea type="text" class="form-control rounded-0" id="comment" rows="1" wire:model="comment"></textarea>
+                    <textarea type="text" class="form-control" id="comment" rows="2" wire:model="comment"></textarea>
                         <ul class="text-muted well well-sm shadow-none">
                             <li>- Max Length: 500 characters</li>
                         </ul>    
@@ -281,9 +299,111 @@
 
 </div>
 
+@push('ex-personnel-js')
+<script>
+    $(document).ready(function() {
+        $('#personnel').select2();
+        $('#personnel').on('change', function (e) {
+            var data = $('#personnel').select2("val");
+            @this.set('personnel', data);
+        });
+    });
+</script>
+@endpush
+
+@push('ex-doc_type-js')
+<script>
+    $(document).ready(function() {
+        $('#doc_type').select2();
+        $('#doc_type').on('change', function (e) {
+            var data = $('#doc_type').select2("val");
+            @this.set('doc_type', data);
+        });
+    });
+</script>
+@endpush
+
+@push('ex-doc_class-js')
+<script>
+    $(document).ready(function() {
+        $('#doc_class').select2();
+        $('#doc_class').on('change', function (e) {
+            var data = $('#doc_class').select2("val");
+            @this.set('doc_class', data);
+        });
+    });
+</script>
+@endpush
+
+@push('ex-mode_of_trans-js')
+<script>
+    $(document).ready(function() {
+        $('#mode_of_trans').select2();
+        $('#mode_of_trans').on('change', function (e) {
+            var data = $('#mode_of_trans').select2("val");
+            @this.set('mode_of_trans', data);
+        });
+    });
+</script>
+@endpush
+
+@push('s_profile-js')
+<script>
+    $(document).ready(function() {
+        $('#s_profile').select2();
+        $('#s_profile').on('change', function (e) {
+            var data = $('#s_profile').select2("val");
+            @this.set('s_profile', data);
+        });
+    });
+</script>
+@endpush
+
+@push('received_by-js')
+<script>
+    $(document).ready(function() {
+        $('#received_by').select2();
+        $('#received_by').on('change', function (e) {
+            var data = $('#received_by').select2("val");
+            @this.set('received_by', data);
+        });
+    });
+</script>
+@endpush
+
+<!-- Select2 Dropdown for Department, Office, Div_unit (Working but returning to default style) -->
+<!-- @push('addressed-to-js')
+<script>
+    $(document).ready(function() {
+        $('.form-select').select2();
+        $('#department').on('change', function (e) {
+            console.log(e.target.value);
+            var data = $('#department').select2("val");
+            @this.set('selectedDepartment', data);
+        });
+
+        $('#office').select2();
+        $('#office').on('change', function (e) {
+            console.log(e.target.value);
+            var data = $('#office').select2("val");
+            @this.set('selectedOffice', data);
+        });
+
+        $('#div_unit').select2();
+        $('#div_unit').on('change', function (e) {
+            console.log(e.target.value);
+            var data = $('#div_unit').select2("val");
+            @this.set('div_unit', data);
+        });
+    });
+</script>
+@endpush -->
+
 <script>
     function goBack() {
         window.history.back();
     }
 </script>
+
+
  
